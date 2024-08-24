5f98ad74757983852e66aadb6a93770a0280384f
khushnood956 <khushnoodahmed956@gmail.com>
2024-08-24 20:18:41 +0500

    modified projects

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,40 +2,33 @@ import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        // Input first number
-        System.out.print("Enter First Number: ");
-        int num1 = sc.nextInt();
-        
-        // Input second number
-        System.out.print("Enter Second Number: ");
-        int num2 = sc.nextInt();
-        
-        // Choose operator
-        System.out.print("Choose operator! 1 for add, 2 for subtract, 3 for multiply, 4 for divide: ");
-        int operator = sc.nextInt();
-        
-        // Perform operation
-        switch (operator) {
-            case 1:
-                System.out.println("Result: " + (num1 + num2));
-                break;
-            case 2:
-                System.out.println("Result: " + (num1 - num2));
-                break;
-            case 3:
-                System.out.println("Result: " + (num1 * num2));
-                break;
-            case 4:
-                if (num2 != 0) {
-                    System.out.println("Result: " + (num1 / num2));
-                } else {
-                    System.out.println("Error: Cannot divide by zero!");
+        try (Scanner sc = new Scanner(System.in)) {
+            // Input first number
+            System.out.print("Enter First Number: ");
+            int num1 = sc.nextInt();
+            
+            // Input second number
+            System.out.print("Enter Second Number: ");
+            int num2 = sc.nextInt();
+            
+            // Choose operator
+            System.out.print("Choose operator! 1 for add, 2 for subtract, 3 for multiply, 4 for divide: ");
+            int operator = sc.nextInt();
+            
+            // Perform operation
+            switch (operator) {
+                case 1 -> System.out.println("Result: " + (num1 + num2));
+                case 2 -> System.out.println("Result: " + (num1 - num2));
+                case 3 -> System.out.println("Result: " + (num1 * num2));
+                case 4 -> {
+                    if (num2 != 0) {
+                        System.out.println("Result: " + (num1 / num2));
+                    } else {
+                        System.out.println("Error: Cannot divide by zero!");
+                    }
                 }
-                break;
-            default:
-                System.out.println("Invalid operator selected. Please choose 1, 2, 3, or 4.");
+                default -> System.out.println("Invalid operator selected. Please choose 1, 2, 3, or 4.");
+            }
         }
         
         System.out.println("Thanks for using the calculator!");
--- random.java
+++ random.java
@@ -5,23 +5,24 @@ public class random {
     public static void main(String[] args) {
     System.out.println("Welcome to Random Number Guessing game");
     int Ran_Number = (int)(Math.random()*100);
-    Scanner sc = new Scanner(System.in);
-    int Guessed = sc.nextInt();
-    if (Ran_Number == Guessed )
-    System.out.println("You Guessed Correctly");
-    else if (Ran_Number > Guessed) {
+    try (Scanner sc = new Scanner(System.in)) {
+        int Guessed = sc.nextInt();
+        if (Ran_Number == Guessed )
+        System.out.println("You Guessed Correctly");
+        else if (Ran_Number > Guessed) {
 
-        System.out.println("The number you have guessed is smaller.");
-        System.out.print("Correct number is ");
-        System.out.println(Ran_Number);
-    }
-    else  {
-        System.out.println("The number you have guessed is greater.");
-        System.out.print("Correct number is ");
-        System.out.println(Ran_Number);
+            System.out.println("The number you have guessed is smaller.");
+            System.out.print("Correct number is ");
+            System.out.println(Ran_Number);
+        }
+        else  {
+            System.out.println("The number you have guessed is greater.");
+            System.out.print("Correct number is ");
+            System.out.println(Ran_Number);
 
 
 
+        }
     }
 
     }
