c04597f075d0d166efa7e781c6b1faffaf9f242c
khushnood956 <khushnoodahmed956@gmail.com>
2024-08-28 01:14:03 +0500

    pattern practice file notes using java

---
+++ Main.java
@@ -0,0 +1,125 @@
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Pattern 1");
+//        for(int i = 1; i < 6; i++) {
+//           for( int j=1; j<5; j++) {
+//               System.out.print("* ");
+//           }
+////               System.out.println("*");
+//            System.out.println("*");
+
+
+
+
+        // pattern 2
+//        int n = 4;
+//        int m = 5;
+//        for (int i = 1; i <= n; i++) {
+//            for (int j = 1; j <= m; j++) {
+//                if (i == 1 || j == 1 || i == n || j == m) {
+//                    System.out.print("*");
+//                } else {
+//                    System.out.print(" ");
+//                }
+//            }
+//                System.out.println();
+//        }
+//        System.out.println("*");
+
+
+
+//pattern 3
+
+//        for (int i = 1; i <= 4; i++){
+//            System.out.print(" ");
+//            for (int j = 1; j<=i; j++) {
+//                System.out.print("*");
+//
+//            }
+//            System.out.println();
+
+
+
+
+        //pattern 4
+//        for (int i = 4; i >= 1; i--){
+//            System.out.print(" ");
+//            for (int j = 1; j<=i; j++) {
+//                System.out.print("*");
+//
+//            }
+//            System.out.println();
+
+
+
+
+        //pattern 5
+//        int n = 4;
+//        for (int i = 1; i <= n; i++){
+//            for (int j = 1; j <= n-i; j++) {
+//                System.out.print(" ");
+//            }
+//                for (int j = 1; j <= i; j++){
+//            System.out.print("*");}
+//            System.out.println();
+
+
+
+
+        // pattern 6
+//        int k = 1;
+//        int n = 5;
+//        for (int i = 1; i <= n; i++) {
+////            System.out.print(k);
+//            for (int j = 1; j <= i; j++){
+//                System.out.print(j);}
+//            System.out.println();
+
+
+
+
+        //pattern 7
+//                int k = 1;
+//        int n = 5;
+//        for (int i = n; i >= 1; i--) {
+////            System.out.print(k);
+//            for (int j = 1; j <= i; j++){
+//                System.out.print(j);}
+//            System.out.println();
+
+
+
+
+        //pattern 8 floyd pattern..
+//    int n = 5;
+//    int k = 1;
+//    for (int i = 1; i <= n; i++) {
+//
+//        for (int j = 1; j <= i; j++) {
+//            System.out.print(k);
+//            System.out.print(" ");
+//            k++;
+//        }
+//        System.out.println();
+//    }
+
+
+        // 0 1 triangle pattern 9
+//        int n = 5;
+//        for (int i = 1; i<= n; i++){
+//            for (int j = 1; j <= i; j++){
+//                if ((i + j) % 2 == 0)
+//                    System.out.print("1");
+//            else
+//                    System.out.print(0);}
+//
+//        System.out.println();
+        }
+        }
+        }
+
+
+
+
+
+
