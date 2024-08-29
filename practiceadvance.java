6c2b8efe9730b39d8d589a08a74c4b8dd8910d31
khushnood956 <khushnoodahmed956@gmail.com>
2024-08-30 01:51:57 +0500

    advance patternquestion

---
+++ practiceadvance.java
@@ -0,0 +1,152 @@
+
+public class practiceadvance {
+    public static void main(String[] args) {
+//        int n = 4;
+//        for (int i = 1; i <= n; i++) {
+//            for (int j = 1; j <= i; j++) {
+//                System.out.print("*");}
+//                for (int j=2*(n-i); j>=1; j-- ){
+//                    System.out.print(" ");}
+//                for (int j = 1; j <= i; j++){
+//                    System.out.print("*");
+//                }
+//            System.out.println();
+//        }
+//        for (int i = n; i >= 1; i--) {
+//            for (int j = 1; j <= i; j++) {
+//                System.out.print("*");}
+//            for (int j=2*(n-i); j>=1; j-- ){
+//                System.out.print(" ");}
+//            for (int j = 1; j <= i; j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+        // pattern 11
+//        int n = 5;
+//        for (int i = 1; i <= n; i++){
+//            for (int j = n-i; j >= 1; j--){
+//                System.out.print(" ");
+//
+//            }
+//            for (int j = 1; j <= n; j++){
+////                if (i == 1 || i == 5 || j == 1 || j == 5)
+//            System.out.print("*");
+////            else
+////            System.out.print(" ");
+//            }
+//            System.out.println();
+//            // first pattern i draw in 1 try\
+//            // can be convverted to hollow by removing comments
+//
+//
+//        }
+//        int n = 5;
+//        for (int i = 1; i <= n; i++) {
+//            for (int j = 1; j <= n-i; j++) {
+//                System.out.print(" ");
+//
+//            }
+//            for (int j = i; j >= 1; j--) {
+//                System.out.print(i + " ");
+//            }
+//            System.out.println();
+//        }
+
+
+        //palindromic pyramid
+//                int n = 5;
+//        for (int i = 1; i <= n; i++) {
+//            for (int j = 1; j <= n-i; j++) {
+//                System.out.print(" ");
+//
+//            }
+//            for (int j = i; j >= 1; j--) {
+//                System.out.print(j);
+//
+//            }
+//            for (int j = 2; j <= i; j++) {
+//                System.out.print(j);
+//
+//            }
+//            System.out.println();
+//        }
+//diamond pattern
+//        int n = 4;
+//        for (int i = 1; i <= n; i++) {
+//            for (int j = 1; j <= n - i; j++) {
+//                System.out.print(" ");
+//
+//            }
+//            for (int j = 1; j <= 2 * i-1; j++) {
+//                System.out.print("*");
+//
+////            System.out.println();
+//            }
+//
+//            System.out.println();
+//
+//        }
+//        for (int i = n; i >= 1; i--) {
+//            for (int j = 1; j <= n - i; j++) {
+//                System.out.print(" ");
+//
+//            }
+//            for (int j = 2 * i-1; j >= 1; j--) {
+//                System.out.print("*");
+//
+////            System.out.println();
+//            }
+//
+//            System.out.println();
+        // hollow butterfly
+//        int n = 4;
+
+
+        //upper part
+//        for(int i=1; i<=n; i++) {
+//            for(int j=1; j<=i; j++) {
+//                if (j == 1 || j == i)
+//                    System.out.print("*");
+//                else System.out.print(" ");
+//            }
+//
+//
+//            int spaces = 2 * (n-i);
+//            for(int j=1; j<=spaces; j++) {
+//                System.out.print(" ");
+//            }
+//
+//
+//            for(int j=1; j<=i; j++) {
+//                if (j == 1 || j == i)
+//                    System.out.print("*");
+//                else System.out.print(" ");
+//            }
+//            System.out.println();
+//        }
+//        for(int i=n; i>=1; i--) {
+//            for(int j=1; j<=i; j++) {
+//                if (j == 1 || j == i)
+//                    System.out.print("*");
+//                else System.out.print(" ");
+//            }
+//
+//
+//            int spaces = 2 * (n-i);
+//            for(int j=1; j<=spaces; j++) {
+//                System.out.print(" ");
+//            }
+//
+//
+//            for(int j=1; j<=i; j++) {
+//                if (j == 1 || j == i)
+//                    System.out.print("*");
+//                else System.out.print(" ");
+//            }
+//            System.out.println();
+//        }
+        }
+
+        }
